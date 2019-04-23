edec6f017f2a2c602c638a209638cf054262b3a3
Athena Deng <539@holbertonschool.com>
2019-04-23 01:40:14 +0000

    first commit

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""starts Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """displays a message"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays a message"""
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """displays "C" followed value of text variable"""
+    return "C %s" % text
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
