fa363e51f3fcc24c9c3462445e0f597408431239
Athena Deng <539@holbertonschool.com>
2019-04-24 00:17:51 +0000

    int variable

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,7 +39,7 @@ def num_n(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def num_template(n):
     """displays a HTML page if n is an integer"""
-    return render_template('5-number.html')
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
