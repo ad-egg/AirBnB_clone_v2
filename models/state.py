ce082645d2ed4aa1647b8d5e1f0942324e71e59b
Athena Deng <539@holbertonschool.com>
2019-04-24 19:50:30 +0000

    added if/else statement

--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ import models
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -11,18 +12,19 @@ class State(BaseModel, Base):
     Attributes:
         name: input name
     """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-
-    cities = relationship(
-            "City", backref='state', cascade="all, delete, delete-orphan")
-
-    @property
-    def cities(self):
-        """ gets a list of city objects with that state id """
-        all_obj = models.storage.all()
-        cities_list = []
-        for k, v in all_obj.items():
-            if v.__class__.__name__ == 'City' and v.state_id == self.id:
-                cities_list.append(v)
-        return cities_list
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship(
+                "City", backref='state', cascade="all, delete, delete-orphan")
+    else:
+        name = ""
+        @property
+        def cities(self):
+            """ gets a list of city objects with that state id """
+            all_obj = models.storage.all()
+            cities_list = []
+            for k, v in all_obj.items():
+                if v.__class__.__name__ == 'City' and v.state_id == self.id:
+                    cities_list.append(v)
+            return cities_list
