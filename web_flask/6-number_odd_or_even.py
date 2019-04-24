d7e35263b3e2ce7181605d1c0a7e6cfe51ba038a
Athena Deng <539@holbertonschool.com>
2019-04-24 07:14:35 +0000

    removed quotes

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -48,4 +48,4 @@ def odd_even_num(n):
     return render_template('6-number_odd_or_even.html', n=n)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
